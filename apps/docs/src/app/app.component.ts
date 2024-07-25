3ff2bc12f0cfc69ce77b468371cbca0ffa05f2e8
NAJIH Driss <najih.driss73@gmail.com>
2024-07-25 16:47:03 +0200

    successfully configure nx with angular package and docs

--- apps/docs/src/app/app.component.ts
+++ apps/docs/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import {NgxComponent} from "@kouji/ngx";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, NgxComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
---
+++ packages/angular/README.md
@@ -0,0 +1,7 @@
+# @kouji/ngx
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @kouji/ngx` to execute the unit tests.
